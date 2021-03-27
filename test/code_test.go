5b5cb2a1a8c49a87c19e559928917f6e94aedaf7
wangyixiang <wangyixiang@didichuxing.com>
2021-03-27 15:02:31 +0800

    channel test

--- test/code_test.go
+++ test/code_test.go
@@ -2,7 +2,12 @@ package test
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
 
 /**
@@ -38,4 +43,157 @@ func TestSubArrayTotalMax(t *testing.T) {
 
 	fmt.Println()
 	fmt.Println(max)
+}
+
+func TestTimer(t *testing.T) {
+	timer1 := time.NewTimer(time.Second * 2)
+	t1 := time.Now() //当前时间
+	fmt.Println(t1)
+	t2 := <-timer1.C
+	fmt.Println(t2)
+
+	//如果只是想单纯的等待的话，可以使用 time.Sleep 来实现
+	timer2 := time.NewTimer(time.Second * 2)
+	<-timer2.C
+	fmt.Println("2s后")
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("再一次2s后")
+
+	<-time.After(time.Second * 2)
+	fmt.Println("再再一次2s后")
+
+	timer3 := time.NewTimer(time.Second)
+	go func() {
+		<-timer3.C
+		fmt.Println("Timer 3 expired")
+	}()
+
+	stop := timer3.Stop() //停止定时器
+	if stop {
+		fmt.Println("Timer 3 stopped")
+	}
+
+	fmt.Println("before")
+	timer4 := time.NewTimer(time.Second * 5) //原来设置3s
+	timer4.Reset(time.Second * 1)            //重新设置时间
+	<-timer4.C
+	fmt.Println("after")
+}
+
+func TestChannel(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+
+	// ...
+	const MaxRandomNumber = 100000
+	const NumReceivers = 10
+	const NumSenders = 1000
+
+	wgReceivers := sync.WaitGroup{}
+	wgReceivers.Add(NumReceivers)
+
+	// ...
+	dataCh := make(chan int, 100)
+	stopCh := make(chan struct{})
+	// stopCh is an additional signal channel.
+	// Its sender is the moderator goroutine shown below.
+	// Its reveivers are all senders and receivers of dataCh.
+	toStop := make(chan string, 1)
+	// The channel toStop is used to notify the moderator
+	// to close the additional signal channel (stopCh).
+	// Its senders are any senders and receivers of dataCh.
+	// Its reveiver is the moderator goroutine shown below.
+
+	var stoppedBy string
+
+	// moderator
+	go func() {
+		stoppedBy = <-toStop
+		close(stopCh)
+	}()
+
+	// senders
+	for i := 0; i < NumSenders; i++ {
+		go func(id string) {
+			for {
+				value := rand.Intn(MaxRandomNumber)
+				if value == 0 {
+					// Here, a trick is used to notify the moderator
+					// to close the additional signal channel.
+					select {
+					case toStop <- "sender#" + id:
+					default:
+					}
+					return
+				}
+
+				// The first select here is to try to exit the goroutine
+				// as early as possible. This select blocks with one
+				// receive operation case and one default branches will
+				// be optimized as a try-receive operation by the
+				// official Go compiler.
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				// Even if stopCh is closed, the first branch in the
+				// second select may be still not selected for some
+				// loops (and for ever in theory) if the send to
+				// dataCh is also unblocked.
+				// This is why the first select block is needed.
+				select {
+				case <-stopCh:
+					return
+				case dataCh <- value:
+				}
+			}
+		}(strconv.Itoa(i))
+	}
+
+	// receivers
+	for i := 0; i < NumReceivers; i++ {
+		go func(id string) {
+			defer wgReceivers.Done()
+
+			for {
+				// Same as the sender goroutine, the first select here
+				// is to try to exit the goroutine as early as possible.
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				// Even if stopCh is closed, the first branch in the
+				// second select may be still not selected for some
+				// loops (and for ever in theory) if the receive from
+				// dataCh is also unblocked.
+				// This is why the first select block is needed.
+				select {
+				case <-stopCh:
+					return
+				case value := <-dataCh:
+					if value == MaxRandomNumber-1 {
+						// The same trick is used to notify
+						// the moderator to close the
+						// additional signal channel.
+						select {
+						case toStop <- "receiver#" + id:
+						default:
+						}
+						return
+					}
+
+					log.Println(value)
+				}
+			}
+		}(strconv.Itoa(i))
+	}
+
+	// ...
+	wgReceivers.Wait()
+	log.Println("stopped by", stoppedBy)
 }
